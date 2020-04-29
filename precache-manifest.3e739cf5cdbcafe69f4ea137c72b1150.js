3899559ec05a2791f3c46c8eb042dad9943de002
NelsonBlakeN <nelson.blaken@gmail.com>
2020-04-29 11:13:01 -0700

    Updates

---
+++ precache-manifest.3e739cf5cdbcafe69f4ea137c72b1150.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a3d2748a1d4e4c104c10ee1b4ccb982",
+    "url": "/Housekeeping/index.html"
+  },
+  {
+    "revision": "1d3d3f00af58c45a3fa6",
+    "url": "/Housekeeping/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "c067576510c53eff2909",
+    "url": "/Housekeeping/static/js/2.0857f0d0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Housekeeping/static/js/2.0857f0d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d3d3f00af58c45a3fa6",
+    "url": "/Housekeeping/static/js/main.848124d6.chunk.js"
+  },
+  {
+    "revision": "9e69adaf1ee1e0364883",
+    "url": "/Housekeeping/static/js/runtime-main.7464fd40.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Housekeeping/static/media/logo.5d5d9eef.svg"
+  }
+]);
